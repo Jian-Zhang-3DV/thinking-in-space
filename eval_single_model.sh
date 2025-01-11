d3b71b4f1850dfa975293a2093ce06df98e2f655
zjrandom951 <zjrandomyeah@gmail.com>
2025-01-11 09:57:18 +0000

    Update model checkpoint path in eval_single_model.sh to use the latest version

--- eval_single_model.sh
+++ eval_single_model.sh
@@ -2,7 +2,7 @@ benchmark="vsibench"
 model="llava_one_vision_qwen2_7b_ov_32f"
 output_path=logs/$(TZ="America/New_York" date "+%Y%m%d")
 model_family="llava_onevision"
-model_args="pretrained=LLaVA-NeXT/work_dirs/llavanext-google_siglip-so400m-patch14-384-Qwen_Qwen2-7B-Instruct-spann3r/checkpoint-1031,\
+model_args="pretrained=LLaVA-NeXT/work_dirs/llavanext-google_siglip-so400m-patch14-384-Qwen_Qwen2-7B-Instruct-spann3r/checkpoint-100,\
 model_base=LLaVA-NeXT/checkpoints/LLaVA-Video-7B-Qwen2,\
 conv_template=qwen_1_5,\
 model_name=llava_qwen,\
