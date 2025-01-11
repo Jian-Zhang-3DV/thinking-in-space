698819168b592cee03751d5cedb1cf38a31e997d
zjrandom951 <zjrandomyeah@gmail.com>
2025-01-11 14:44:52 +0000

    Refactor model_args in eval_single_node.sh for improved readability and JSON syntax
    - Split model_args into multiple lines for better clarity
    - Update model_args to use single quotes for compatibility with JSON formatting
    - Ensure overwrite_config parameters are clearly defined and maintain valid JSON structure

--- eval_single_node.sh
+++ eval_single_node.sh
@@ -4,9 +4,11 @@ benchmark="vsibench"
 model="llava_one_vision_qwen2_7b_ov_32f"
 output_path=logs/$(TZ="America/New_York" date "+%Y%m%d")
 model_family="llava_onevision"
-model_args='\pretrained=LLaVA-NeXT/work_dirs/llavanext-google_siglip-so400m-patch14-384-Qwen_Qwen2-7B-Instruct-spann3r/checkpoint-150,\
+model_args='pretrained=LLaVA-NeXT/work_dirs/llavanext-google_siglip-so400m-patch14-384-Qwen_Qwen2-7B-Instruct-spann3r/checkpoint-150,\
 conv_template=qwen_1_5,\
-overwrite_config={tie_word_embeddings=false,use_cache=true,vocab_size=152064},\
+tie_word_embeddings=false,\
+use_cache=true,\
+vocab_size=152064,\
 model_name=llava_qwen,\
 max_frames_num=32'
 
