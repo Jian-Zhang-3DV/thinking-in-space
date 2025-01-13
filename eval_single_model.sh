3d41cdaaf2eca05ec049ca874711cfc8b093a55f
zjrandom951 <zjrandomyeah@gmail.com>
2025-01-13 10:49:07 +0000

    Refactor eval_single_model.sh for improved evaluation configuration
    - Ensure consistent export of LMMS_EVAL_LAUNCHER variable for clarity in execution context.
    - Comment out alternative launch command for flexibility in model evaluation options.
    - These changes aim to enhance the usability and maintainability of the evaluation script.

--- eval_single_model.sh
+++ eval_single_model.sh
@@ -8,8 +8,8 @@ conv_template=qwen_1_5,\
 model_name=llava_qwen,\
 max_frames_num=32"
 # model_args="pretrained=LLaVA-NeXT/work_dirs/llavanext-google_siglip-so400m-patch14-384-Qwen_Qwen2-7B-Instruct-ov_to_video_am9,conv_template=qwen_1_5,model_name=llava_qwen,max_frames_num=32"
-export LMMS_EVAL_LAUNCHER="accelerate"
 
+export LMMS_EVAL_LAUNCHER="accelerate"
 accelerate launch \
     --num_processes=4 \
     -m lmms_eval \
@@ -20,3 +20,13 @@ accelerate launch \
     --log_samples \
     --log_samples_suffix $model \
     --output_path $output_path/$benchmark
+
+# export LMMS_EVAL_LAUNCHER="python"
+# python -m lmms_eval \
+#     --model $model_family \
+#     --model_args $model_args \
+#     --tasks $benchmark \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix $model \
+#     --output_path $output_path/$benchmark
