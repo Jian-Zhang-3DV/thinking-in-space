bfc995ba9138b0991572995c86e43a025804c558
Jian-Zhang-3DV <951554359@qq.com>
2025-05-07 09:42:51 -0900

    Update checkpoint entry in SLURM submission script to new model version

--- submit_all_slurm.py
+++ submit_all_slurm.py
@@ -41,7 +41,7 @@ MODELS_and_CONFIGS = [
     # ("llava_video_7b_qwen2_05_02_lora_patch_tokens_cross_attn_2_layers_mlp_diff_lr/checkpoint-1400", "lmms-lab/LLaVA-NeXT-Video-7B-Qwen2", 16, "01:00:00"), # Added example nodes and time
     # ("llava_video_7b_qwen2_05_06_cut3r_all_tokens_cross_attn_lora_diff_lr/checkpoint-1400", "lmms-lab/LLaVA-NeXT-Video-7B-Qwen2", 16, "01:00:00"), # Added example nodes and time
     # ("llava_video_7b_qwen2_05_06_cut3r_all_tokens_cross_attn_lora/checkpoint-1400", "lmms-lab/LLaVA-NeXT-Video-7B-Qwen2", 16, "01:00:00"), # Added example nodes and time
-    ("llava_video_7b_qwen2_05_06_vggt_all_tokens_cross_attn_lora/checkpoint-2100", "lmms-lab/LLaVA-NeXT-Video-7B-Qwen2", 16, "01:00:00"), # Added example nodes and time
+    ("llava_video_7b_qwen2_05_07_vggt_all_tokens_cross_attn_lora_6tasks/checkpoint-2100", "lmms-lab/LLaVA-NeXT-Video-7B-Qwen2", 16, "01:00:00"), # Added example nodes and time
 
 ]
 BENCHMARKS = [
