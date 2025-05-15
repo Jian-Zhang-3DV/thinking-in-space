4cb755907f62fd85efb04f8acb76ad4ffaf1e060
Junge Zhang <jzhan744@skylark.ib.hpcc.ucr.edu>
2025-05-15 10:57:21 -0700

    update gemini 2.0 flash

--- eval_gemini.sh
+++ eval_gemini.sh
@@ -1,5 +1,3 @@
-# bash evaluate_all_in_one.sh --model all --num_processes 8 --benchmark vsibench
-# bash evaluate_all_in_one.sh --model all --num_processes 2 --benchmark vstrbench
 
 #!/bin/bash
 
@@ -10,7 +8,7 @@
 task="vstibench"
 
 # Output file
-output_file="all_tasks_output_gemini-2.5.txt"
+output_file="all_tasks_output_gemini.txt"
 
 # Clear the output file at the start
 > "$output_file"
@@ -19,8 +17,13 @@ output_file="all_tasks_output_gemini-2.5.txt"
 echo "Evaluating task: $task" | tee -a "$output_file"
 # bash evaluate_all_in_one.sh --model gemini_2p0_flash_exp --num_processes 1 --benchmark "$task" >> "$output_file"
 
+# gemini2.0-flash
+bash evaluate_all_in_one.sh --model gemini_2p0_flash_exp --num_processes 8 --benchmark $task >> "$output_file"
+
+# gemini2.5-flash
 bash evaluate_all_in_one.sh --model gemini-2.5-flash-preview-04-17 --num_processes 8 --benchmark $task >> "$output_file"
 
+# gemini2.5-pro
 bash evaluate_all_in_one.sh --model gemini-2.5-pro-preview-05-06 --num_processes 8 --benchmark $task >> "$output_file"
 
 
